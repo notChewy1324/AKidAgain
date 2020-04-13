6da75e674f605c1a9b01b619b71712baef4ce592
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-04-13 12:54:42 -0400

    Update PhotosViewController.swift

--- AKidAgain/Extras/PhotosViewController.swift
+++ AKidAgain/Extras/PhotosViewController.swift
@@ -7,16 +7,36 @@
 //
 
 import UIKit
+import GoogleMobileAds
+import FirebaseCrashlytics
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, GADInterstitialDelegate {
 
     @IBOutlet weak var titleText: UINavigationItem!
     @IBOutlet weak var Photos: UIWebView!
+    var interstitial: GADInterstitial!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //MARK: -FIX ME
+        //Real Ads: ca-app-pub-4600989320659230/6809684574
+        //Test Ads: ca-app-pub-3940256099942544/4411468910
+        self.interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        self.interstitial.delegate = self
+        
+        let request = GADRequest()
+        self.interstitial.load(request)
+        
         let url = URL(string: "https://akidagain.smugmug.com/")
         Photos.loadRequest(URLRequest(url: url!))
+        
+        if self.interstitial.isReady {
+            self.interstitial.present(fromRootViewController: self)
+        } else {
+          //Do nothin
+        }
+        
     }
     
     @IBAction func doSomething(_ sender: AnyObject) {
