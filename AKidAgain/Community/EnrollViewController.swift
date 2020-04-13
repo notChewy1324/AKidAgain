bda3721fabd0fcd8b798e9a205f7b3fc01d381b2
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-04-13 12:54:45 -0400

    Update EnrollViewController.swift

--- AKidAgain/Community/EnrollViewController.swift
+++ AKidAgain/Community/EnrollViewController.swift
@@ -8,16 +8,37 @@
 
 import UIKit
 import Firebase
-class EnrollViewController: UIViewController {
+import GoogleMobileAds
+import FirebaseCrashlytics
+class EnrollViewController: UIViewController, GADInterstitialDelegate {
 
     @IBOutlet weak var Photos: UIWebView!
     @IBOutlet weak var titleText: UINavigationItem!
+    var interstitial: GADInterstitial!
     
     @IBAction func RefreshButton(_ sender: Any) {
+        
+        //MARK: -FIX ME
+        //Real Ads: ca-app-pub-4600989320659230/6809684574
+        //Test Ads: ca-app-pub-3940256099942544/4411468910
+        self.interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        self.interstitial.delegate = self
+        
+        let request = GADRequest()
+        self.interstitial.load(request)
+        
         setupRemoteConfigDefaults()
         fecthRemoteConfig()
         updateViewWithRCValues()
+        
+        if self.interstitial.isReady {
+            self.interstitial.present(fromRootViewController: self)
+        } else {
+          //Do nothin
+        }
+
     }
+    
     //remote-config
     func updateViewWithRCValues(){
         //remote config values here
@@ -44,8 +65,25 @@ class EnrollViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //MARK: -FIX ME
+        //Real Ads: ca-app-pub-4600989320659230/6809684574
+        //Test Ads: ca-app-pub-3940256099942544/4411468910
+        self.interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        self.interstitial.delegate = self
+        
+        let request = GADRequest()
+        self.interstitial.load(request)
+        
         setupRemoteConfigDefaults()
         fecthRemoteConfig()
         updateViewWithRCValues()
+        
+        if self.interstitial.isReady {
+            self.interstitial.present(fromRootViewController: self)
+        } else {
+          //Do nothin
+        }
+        
     }
 }
