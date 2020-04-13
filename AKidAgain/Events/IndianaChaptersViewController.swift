7b7ebede2c7cd4400ce6bb3ef517192665b1c3d6
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-04-13 16:17:35 -0400

    Update IndianaChaptersViewController.swift

--- AKidAgain/Events/IndianaChaptersViewController.swift
+++ AKidAgain/Events/IndianaChaptersViewController.swift
@@ -7,11 +7,44 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseCrashlytics
 class IndianaChaptersViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    //remote-config
+    func updateViewWithRCValues(){
+        
+        //remote config values here
+        let eventName = RemoteConfig.remoteConfig().configValue(forKey: "IndianaChapterEvent").stringValue ?? ""
+        label.text = (eventName)
+    }
+    
+    func setupRemoteConfigDefaults(){
+        let defaultValues = [
+            "IndianaChapterEvent" : "Loading..." as NSObject
+        ]
+        RemoteConfig.remoteConfig().setDefaults(defaultValues)
+    }
+    
+    func fecthRemoteConfig(){
+        RemoteConfig.remoteConfig().fetch(withExpirationDuration: 3600) { [unowned self] (status, error) in
+            guard error == nil else {
+                Crashlytics.crashlytics().record(error: error!)
+                return
+            }
+            print("Recived values from the cloud!")
+            RemoteConfig.remoteConfig().fetchAndActivate()
+            self.updateViewWithRCValues()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupRemoteConfigDefaults()
+        fecthRemoteConfig()
+        updateViewWithRCValues()
     }
     
     @IBAction func doSomething(_ sender: AnyObject) {
