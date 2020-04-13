961daec15024abe99bc01c4340e738e227e7b7b6
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-04-13 16:17:06 -0400

    Create SouthernChapterViewController.swift

---
+++ AKidAgain/Events/OhioChapters/SouthernChapterViewController.swift
@@ -0,0 +1,54 @@
+//
+//  SouthernChapterViewController.swift
+//  AKidAgain
+//
+//  Created by Cameron Garrison on 4/13/20.
+//  Copyright © 2020 NotChewy1324. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseCrashlytics
+class SouthernChapterViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    //remote-config
+    func updateViewWithRCValues(){
+        
+        //remote config values here
+        let eventName = RemoteConfig.remoteConfig().configValue(forKey: "SouthernOhioChapterEvent").stringValue ?? ""
+        label.text = (eventName)
+    }
+    
+    func setupRemoteConfigDefaults(){
+        let defaultValues = [
+            "SouthernOhioChapterEvent" : "Loading..." as NSObject
+        ]
+        RemoteConfig.remoteConfig().setDefaults(defaultValues)
+    }
+    
+    func fecthRemoteConfig(){
+        RemoteConfig.remoteConfig().fetch(withExpirationDuration: 3600) { [unowned self] (status, error) in
+            guard error == nil else {
+                Crashlytics.crashlytics().record(error: error!)
+                return
+            }
+            print("Recived values from the cloud!")
+            RemoteConfig.remoteConfig().fetchAndActivate()
+            self.updateViewWithRCValues()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupRemoteConfigDefaults()
+        fecthRemoteConfig()
+        updateViewWithRCValues()
+    }
+    
+    @IBAction func doSomething(_ sender: AnyObject) {
+        ClickSound()
+    }
+
+}
